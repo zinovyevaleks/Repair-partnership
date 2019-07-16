4e2568fac0baa3d0f5dc63552558b931e13b5983
Александр <zinovyevaleks@gmail.com>
2019-07-16 17:49:57 +0700

    Доделал вёрстку

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
-$(document).ready( function(){
-    // Навигация
-
+$(document).ready(function () {
+  $('.slider-1').slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    variableWidth: true
   });
+  $('.slider-2').slick({
+    dots: true,
+    infinite: true,
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 1,
+    variableWidth: true
+  });
+
+});
