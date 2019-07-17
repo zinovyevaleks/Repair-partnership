7af582eb07131a6527b4b138312f9ffc192c8b9c
Александр <zinovyevaleks@gmail.com>
2019-07-17 17:58:43 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -22,4 +22,22 @@ $(document).ready(function () {
 
   $(".phone-mask").mask("+7-___-__-__");
 
+  	//E-mail Ajax Send
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Спасибо за заявку!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+
 });
