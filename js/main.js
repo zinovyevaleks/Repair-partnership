e527e5281f1aa4a59ed9a504e73c7a0bf213c8a1
Александр <zinovyevaleks@gmail.com>
2019-07-18 14:24:06 +0700

    переделал slick slider на owl carousel

--- js/main.js
+++ js/main.js
@@ -2,42 +2,73 @@ $(document).ready(function () {
 
   // Карусели
 
-  $('.slider-1').slick({
-    dots: true,
-    infinite: true,
-    slidesToShow: 1,
-    adaptiveHeight: true,
-    variableWidth: true
-  });
-  $('.slider-2').slick({
-    dots: true,
-    infinite: true,
-    centerMode: true,
-    // centerPadding: '60px',
-    slidesToShow: 5,
-    variableWidth: true
-  });
+
+  // $('.slider-2').slick({
+  //   dots: true,
+  //   infinite: true,
+  //   centerMode: true,
+   
+  //   slidesToShow: 5,
+  //   variableWidth: true
+  // });
+
+  $('.results-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    // nav:true,
+    autoHeight:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+
+$('.examples-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  autoHeight:true,
+  autoWidth:true,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:1
+      },
+      1000:{
+          items:1
+      }
+  }
+})
+  
 
   // маска телефона
 
-  $(".phone-mask").mask("+7-___-__-__");
+  $(".phone-mask").mask("+7 (999) 999-99-99");
 
-  	//E-mail Ajax Send
-	$("form").submit(function() { //Change
-		var th = $(this);
-		$.ajax({
-			type: "POST",
-			url: "mail.php", //Change
-			data: th.serialize()
-		}).done(function() {
-			alert("Спасибо за заявку!");
-			setTimeout(function() {
-				// Done Functions
-				th.trigger("reset");
-			}, 1000);
-		});
-		return false;
-	});
+  //E-mail Ajax Send
+  $("#form").submit(function () { //Change
+    var th = $(this);
+    $.ajax({
+      type: "POST",
+      url: "mail.php", //Change
+      data: th.serialize()
+    }).done(function () {
+      alert("Спасибо за заявку!");
+      setTimeout(function () {
+        // Done Functions
+        th.trigger("reset");
+      }, 1000);
+    });
+    return false;
+  });
 
 
 });
